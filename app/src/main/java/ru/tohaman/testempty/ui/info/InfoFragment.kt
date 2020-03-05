c6f3358113fb8aa2592aeb9a55cefd180c83b135
TohaRG2 <avrozovu@yandex.ru>
2020-03-05 22:57:17 +0300

    Доделаны табы для viewPager

--- app/src/main/java/ru/tohaman/testempty/ui/info/InfoFragment.kt
+++ app/src/main/java/ru/tohaman/testempty/ui/info/InfoFragment.kt
@@ -1,11 +1,14 @@
 package ru.tohaman.testempty.ui.info
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_info.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import ru.tohaman.testempty.R
@@ -21,6 +24,17 @@ class InfoFragment : Fragment() {
 
         val vp =  view.findViewById<ViewPager2>(R.id.infoViewPager)
         vp.adapter = InfoPagerAdapter(this)
+
+        var tabLayout = view.findViewById<TabLayout>(R.id.tabLayout)
+        TabLayoutMediator(tabLayout, vp) { tab, position ->
+            var text = ""
+            when (position) {
+                0 -> text = "О программе"
+                1 -> text = "Спасибо"
+                2 -> text = "Помощь"
+            }
+            tab.text = text
+        }.attach()
 
         return view
     }
