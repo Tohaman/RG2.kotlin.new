2aae57799e454d86a9f789f3ec0c6832c2583a51
TohaRG2 <avrozovu@yandex.ru>
2020-03-24 23:08:13 +0300

    Диалог азбуки

--- app/src/main/java/ru/tohaman/testempty/ui/learn/RecyclerDialog.kt
+++ app/src/main/java/ru/tohaman/testempty/ui/learn/RecyclerDialog.kt
@@ -4,5 +4,5 @@ import android.content.Context
 import androidx.appcompat.app.AlertDialog
 
 class RecyclerDialog(context: Context) : AlertDialog(context) {
-
+    //https://startandroid.ru/ru/uroki/vse-uroki-spiskom/180-urok-110-android-3-fragments-dialogfragment-dialog.html
 }
