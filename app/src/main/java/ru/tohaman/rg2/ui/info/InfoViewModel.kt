6fcfa8fd69ed56d62cbcbcd75504364ed2222089
Tohaman <avrozovu@yandex.ru>
2020-07-27 20:53:39 +0300

    R2-6 Добавить окно с историей версий по нажатию на какой-нибудь элемент в "О программе"

--- app/src/main/java/ru/tohaman/rg2/ui/info/InfoViewModel.kt
+++ app/src/main/java/ru/tohaman/rg2/ui/info/InfoViewModel.kt
@@ -7,7 +7,9 @@ import androidx.lifecycle.AndroidViewModel
 import org.koin.core.KoinComponent
 import org.koin.core.get
 import org.koin.core.inject
+import ru.tohaman.rg2.BuildConfig
 import ru.tohaman.rg2.Constants.INFO_BOOKMARK
+import ru.tohaman.rg2.Constants.PAYED_COINS
 import ru.tohaman.rg2.DebugTag.TAG
 import ru.tohaman.rg2.R
 import ru.tohaman.rg2.dataSource.ItemsRepository
@@ -31,7 +33,9 @@ class InfoViewModel(app: Application): AndroidViewModel(app), KoinComponent {
 
     val textAbout = ObservableInt(R.string.about)
 
+    val textThanks = ObservableInt(
+        if (sp.getInt(PAYED_COINS, 0) == 0)  R.string.please_donate else R.string.big_thanks
+    )
 
-
-    val textThanks = ObservableInt(R.string.thanks)
+    val currentVersion = "ver. ${BuildConfig.VERSION_NAME}"
 }
