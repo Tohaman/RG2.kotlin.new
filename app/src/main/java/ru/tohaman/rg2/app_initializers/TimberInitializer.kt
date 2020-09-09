939eaaef06b710cfa62adea9aebdecdaaebaf136
Tohaman <avrozovu@yandex.ru>
2020-09-09 22:05:45 +0300

    R2-15 При выпуске новой версии обновить список в истории

--- app/src/main/java/ru/tohaman/rg2/app_initializers/TimberInitializer.kt
+++ app/src/main/java/ru/tohaman/rg2/app_initializers/TimberInitializer.kt
@@ -2,11 +2,15 @@ package ru.tohaman.rg2.app_initializers
 
 import android.content.Context
 import androidx.startup.Initializer
+import ru.tohaman.rg2.BuildConfig
 import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 class TimberInitializer: Initializer<Unit> {
     override fun create(context: Context) {
-        Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> {
