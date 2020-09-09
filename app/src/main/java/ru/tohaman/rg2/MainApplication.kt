9b769245649a37e8f0e3ad67552b181b9c053297
Tohaman <avrozovu@yandex.ru>
2020-09-09 22:01:38 +0300

    R2-15 При выпуске новой версии обновить список в истории

--- app/src/main/java/ru/tohaman/rg2/MainApplication.kt
+++ app/src/main/java/ru/tohaman/rg2/MainApplication.kt
@@ -1,17 +1,9 @@
 package ru.tohaman.rg2
 
-import android.app.Application
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidFileProperties
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
+import androidx.multidex.MultiDexApplication
 import ru.tohaman.rg2.dataSource.Domain
-import ru.tohaman.rg2.koin.KoinStarter
-import ru.tohaman.rg2.koin.appModule
-import ru.tohaman.rg2.koin.viewModelsModule
-import timber.log.Timber
 
-class MainApplication : Application() {
+class MainApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         Domain.integrateWith(this)
